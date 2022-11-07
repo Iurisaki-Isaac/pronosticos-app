53a0de8eeb97c47c341f751a105159a9a49584dd
isaac <isaac.peralta@dasolab.com>
2022-11-07 08:22:38 -0600

    Primera version de interfaz
    Completado la obtención de clientes y productos para su despliegue en controladores

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import json
 
 #handler class
 class Serv(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self):  
         if self.path == '/':
             self.path = '/public/index.html'
         try:            
@@ -25,7 +25,23 @@ class Serv(BaseHTTPRequestHandler):
 
                 content_len = int(self.headers.get('Content-Length'))
                 post_body = self.rfile.read(content_len)            
-                response = processing.filt(json.loads(post_body))            
+                response = processing.filt(json.loads(post_body))          
+                self.wfile.write(bytes(response,'utf-8'))
+
+            if self.path.endswith("/obtener-clientes"):
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                
+                response = processing.customers()
+                self.wfile.write(bytes(response,'utf-8'))
+
+            if self.path.endswith("/obtener-productos"):
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                
+                response = processing.products()
                 self.wfile.write(bytes(response,'utf-8'))
         except IOError:
             self.send_error(404,"File not found")
